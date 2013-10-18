1ea7b6e459c9543f5c92d92b5186f80d25a45072
Rory Walker <redrory@gmail.com>
2013-10-18 16:57:33 -0500

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Remimiss::Application.config.secret_key_base = '4a2f087cf8af4405e8a24e6ed9cd8bbd7d92f6a050e09d6e981e4ad845838f5a820532a96e553bd08c2e5c7ea6982bdad2bb04b79047d683b01e6f1ab58fc469'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Remimiss::Application.config.session_store :cookie_store, key: '_remimiss_session'
