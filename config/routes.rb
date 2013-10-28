86298f07080f87c60e84e82a8cb769fa3402bb60
Rory Walker <redrory@gmail.com>
2013-10-28 10:12:38 -0500

    wrap up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Remimiss::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_up: "join", sign_out: "bye"}
   resources :visitors, only: [:new, :create]
   root :to => 'visitors#new'
 end
